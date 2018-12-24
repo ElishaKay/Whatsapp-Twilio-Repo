eb04554387d23cfd4478fa3fdda85e50be8310a8
elishakay <kramer1346@gmail.com>
2018-12-24 17:04:35 +0200

    succesfully sends message to users that opt-in over the past 24 hours

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var app = express();
 
+const accountSid = process.env.accountSid;
+const authToken = process.env.authToken;
+const client = require('twilio')(accountSid, authToken);
+
+client.messages
+      .create({
+        body: 'How you doin today?!',
+        from: 'whatsapp:+14155238886',
+        to: 'whatsapp:+972533308841'
+      })
+      .then(message => console.log(message.sid))
+      .done();
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
